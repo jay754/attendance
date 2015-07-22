b001df3db989fb8702c9ba304f1cf62ea87b4f84
Jay Engineer <jayengineer6@gmail.com>
2015-07-22 02:14:49 -0400

    code refactoring

--- src/index.py
+++ src/index.py
@@ -1,6 +1,6 @@
-from bs4 import BeautifulSoup as bs
 from Exceptions import *
 
+from bs4 import BeautifulSoup as bs
 import json
 import urllib2
 import requests
@@ -11,48 +11,42 @@ class Attendance(object):
         self.url = "http://espn.go.com/nba/attendance/_/year/2013"
 
     def check_request(self, url):
-        """
-        To check the http status of the url
-        """
-
         return requests.get(url).status_code
 
     def get_data(self):
-        """
-        Getting the Actual Data from the Espn Url
-        """
-
         http_status = self.check_request(self.url)
+        soup = bs(urllib2.urlopen(self.url), "html.parser")
+        data = [str(i.get_text()) for i in soup.find_all("td")]
 
         if http_status is not None and http_status == 200:
-            soup = bs(urllib2.urlopen(self.url), "html.parser")
-            data = [str(i.get_text()) for i in soup.find_all("td")]
             return data[17:]
+        elif http_status != 200:
+            raise Exception("error with getting data")
+        else:
+            return None
 
     def get_nums(self):
-
-        if self.get_data() is not None: nums = [i.replace(" ", "") for i in self.get_data() if not i.isalpha()]
+        if self.get_data() is not None: nums = [i.replace(" ", "") \
+                           for i in self.get_data() if not i.isalpha()]
 
         return nums
 
     def get_teams(self):
+        if self.get_data() is not None: names = [i.replace(" ", "") \
+                           for i in self.get_data() if i.isalpha()]
 
-        if self.get_data() is not None: names = [i.replace(" ", "") for i in self.get_data() if i.isalpha()]
         names.insert(16, "76ers")
 
         return names
 
     def sanitize_data(self):
-
         striped_comma = [i.replace(",", "") for i in self.get_nums()]
         attendance = [i.replace(".", "") for i in striped_comma]
 
         return attendance
 
     def get_attendance(self):
-
         attendance = set()
-
         for i in self.sanitize_data():
             if i is not "76ers" and int(i) > 500000:
                 attendance.add(i)
@@ -60,7 +54,6 @@ class Attendance(object):
         return attendance
 
     def organize_data(self):
-
         if self.get_teams() is not None:
             teams = self.get_teams()
 
@@ -71,14 +64,3 @@ class Attendance(object):
                 "Attendance": attendance}
 
         return data
-
-def main():
-    try:
-        atteOBJ = Attendance()
-        data = atteOBJ.organizeData()
-        with open('data.json', 'w') as outfile:
-            json.dump(data, outfile)
-    except:
-        raise JsonError
-
-if __name__ == '__main__': main()
