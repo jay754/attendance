2ac86a5f97879f03275060b21a84b26207ddcb60
Jay Engineer <jayengineer6@gmail.com>
2015-07-22 01:52:20 -0400

    this thing is broken

--- src/index.py
+++ src/index.py
@@ -5,18 +5,10 @@ import json
 import urllib2
 import requests
 
-
 class Attendance(object):
 
-    def __init__(self, url=None):
-        self.url = url
-
-    def get_url(self):
-        """
-        A setter for thr url
-        """
-
-        return self.url
+    def __init__(self):
+        self.url = "http://espn.go.com/nba/attendance/_/year/2013"
 
     def check_request(self, url):
         """
@@ -32,36 +24,28 @@ class Attendance(object):
 
         http_status = self.check_request(self.url)
 
-        if http_status is None:
-            return None
-        elif http_status is not None and http_status == 200:
-            soup = bs(urllib2.urlopen(self.url))
+        if http_status is not None and http_status == 200:
+            soup = bs(urllib2.urlopen(self.url), "html.parser")
             data = [str(i.get_text()) for i in soup.find_all("td")]
             return data[17:]
-        else:
-            return -1
 
     def get_nums(self):
 
-        if self.get_data() is not None:
-            nums = [i.replace(" ", "") for i in self.get_data() if not i.isalpha()]
-        
+        if self.get_data() is not None: nums = [i.replace(" ", "") for i in self.get_data() if not i.isalpha()]
+
         return nums
 
     def get_teams(self):
 
-        if self.get_data() is not None:
-            names = [i.replace(" ", "") for i in self.get_data() if i.isalpha()]
-
+        if self.get_data() is not None: names = [i.replace(" ", "") for i in self.get_data() if i.isalpha()]
         names.insert(16, "76ers")
 
         return names
 
     def sanitize_data(self):
 
-        if self.get_nums() is not None:
-            striped_comma = [i.replace(",", "") for i in self.get_nums()]
-            attendance = [i.replace(".", "") for i in striped_comma]
+        striped_comma = [i.replace(",", "") for i in self.get_nums()]
+        attendance = [i.replace(".", "") for i in striped_comma]
 
         return attendance
 
@@ -69,32 +53,28 @@ class Attendance(object):
 
         attendance = set()
 
-        if self.sanitize_data() is not None:
-            for i in self.sanitize_data():
-                if i not "76ers":
-                    if int(i) > 500000:
-                        attendance.add(i)
+        for i in self.sanitize_data():
+            if i is not "76ers" and int(i) > 500000:
+                attendance.add(i)
 
         return attendance
 
     def organize_data(self):
 
-        if self.get_teams() not None:
+        if self.get_teams() is not None:
             teams = self.get_teams()
 
-        if self.get_attendance() not None:
+        if self.get_attendance() is not None:
             attendance = self.get_attendance()
 
-        data = dict(
-                "Teams" : teams,
-                "Attendance": attendance
-                )
+        data = {"Teams" : teams,
+                "Attendance": attendance}
 
         return data
 
 def main():
     try:
-        atteOBJ = attendance()
+        atteOBJ = Attendance()
         data = atteOBJ.organizeData()
         with open('data.json', 'w') as outfile:
             json.dump(data, outfile)
